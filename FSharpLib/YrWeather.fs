377d7e3c2fb41f2aab1b61d4619d6af1915c832e
Rossebø <Hakon.Rossebo@mhwirth.com>
2018-08-17 15:30:43 +0200

    Initial tests

--- FSharpLib/YrWeather.fs
+++ FSharpLib/YrWeather.fs
@@ -1,8 +1,10 @@
-﻿module Yr
+﻿namespace FSharpLib
+
+module Yr =
 
     open FSharp.Data
 
-    type YrForecast = XmlProvider<"./Samples/Yr.xml">
+    type YrForecast = XmlProvider<"../Samples/Yr.xml">
 
     let GetForecastKristiansand() =
             YrForecast.Load("http://www.yr.no/place/Norway/Vest-Agder/Kristiansand/Kristiansand/forecast.xml")
---
+++ FSharpXUnitTests/Tests.fs
@@ -0,0 +1,15 @@
+﻿module Tests
+
+open System
+open Xunit
+open FSharpLib.Yr
+
+[<Fact>]
+let ``My test`` () =
+    Assert.True(true)
+
+
+[<Fact>]
+let ``Test weather hour`` () =
+    let result = GetForecastKristiansand ()
+    Assert.Equal(21, result) 
