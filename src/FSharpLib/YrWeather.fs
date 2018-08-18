b2479020c9567719b1941b54e836c3dbdf7aab5f
Håkon Rossebø <hakonrossebo@hotmail.com>
2018-08-18 18:17:49 +0200

    Moving to src and test folders

--- src/FSharpLib/YrWeather.fs
+++ src/FSharpLib/YrWeather.fs
@@ -4,7 +4,7 @@ module Yr =
 
     open FSharp.Data
 
-    type YrForecast = XmlProvider<"../Samples/Yr.xml">
+    type YrForecast = XmlProvider<"../../Samples/Yr.xml">
 
     let GetForecastKristiansand() =
             YrForecast.Load("http://www.yr.no/place/Norway/Vest-Agder/Kristiansand/Kristiansand/forecast.xml")
