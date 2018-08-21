d2db4456e8cd430f34bfd1ef415cf649bbc350d5
Rossebø <Hakon.Rossebo@mhwirth.com>
2018-08-21 10:43:59 +0200

    Scripting

--- scripting/YrTesting.fsx
+++ scripting/YrTesting.fsx
@@ -1,1 +1,12 @@
-#r "./packages/FSharp.Data/lib/netstandard2.0/FSharp.Data.dll"
+#r "./packages/FSharp.Data/lib/net45/FSharp.Data.dll"
+#r "System.Xml.Linq.dll"
+
+open FSharp.Data
+
+type YrForecast = XmlProvider<"../Samples/Yr.xml">
+
+let KristiansandYrData =
+        YrForecast.Load("http://www.yr.no/place/Norway/Vest-Agder/Kristiansand/Kristiansand/forecast.xml")
+
+for item in KristiansandYrData.Observations.Weatherstations do
+    printfn "%s" item.Name
