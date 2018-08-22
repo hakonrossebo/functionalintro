00fc1420a1592d552ea5ab9c06227e0813d9c9ac
Rossebø <Hakon.Rossebo@mhwirth.com>
2018-08-22 07:24:49 +0200

    Script fix

--- scripting/YrTesting.fsx
+++ scripting/YrTesting.fsx
@@ -5,8 +5,8 @@ open FSharp.Data
 
 type YrForecast = XmlProvider<"../Samples/Yr.xml">
 
-let KristiansandYrData =
-        YrForecast.Load("http://www.yr.no/place/Norway/Vest-Agder/Kristiansand/Kristiansand/forecast.xml")
+let KristiansandYrData = YrForecast.GetSample()
+        // YrForecast.Load("http://www.yr.no/place/Norway/Vest-Agder/Kristiansand/Kristiansand/forecast.xml")
 
 for item in KristiansandYrData.Observations.Weatherstations do
     printfn "%s" item.Name
